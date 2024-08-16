fdd823f887816194762a7846ec7cd700b7deaf7f
Gavan Lamb <gavan.jon.lamb@gmail.com>
2024-08-16 17:35:51 +1000

    feat(GH-1): Add bucket resources

---
+++ buckets.tf
@@ -0,0 +1,274 @@
+resource "aws_s3_bucket" "primary" {
+  provider = aws.primary
+
+  bucket = var.primary_bucket_name
+
+  lifecycle {
+    prevent_destroy = true
+  }
+  
+  tags = var.tags
+}
+resource "aws_s3_bucket_public_access_block" "primary" {
+  provider = aws.primary
+
+  bucket = aws_s3_bucket.primary.id
+  block_public_acls = true
+  block_public_policy = true
+  restrict_public_buckets = true
+  ignore_public_acls = true
+}
+resource "aws_s3_bucket_acl" "primary" {
+  provider = aws.primary
+
+  bucket = aws_s3_bucket.primary.id
+  acl    = "private"
+}
+resource "aws_s3_bucket_versioning" "primary" {
+  provider = aws.primary
+
+  bucket = aws_s3_bucket.primary.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_replication_configuration" "primary" {
+  provider = aws.primary
+  
+  depends_on = [aws_s3_bucket_versioning.primary]
+
+  role   = aws_iam_role.primary_bucket_replication.arn
+  bucket = aws_s3_bucket.primary.id
+
+  rule {
+    id = "all"
+    
+    delete_marker_replication {
+      status = "Enabled"
+    }
+    
+    status = "Enabled"
+
+    destination {
+      bucket        = aws_s3_bucket.secondary.arn
+      storage_class = "STANDARD"
+    }
+  }
+}
+data "aws_iam_policy_document" "primary_bucket_replication_role" {
+  provider = aws.primary
+
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["s3.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+resource "aws_iam_role" "primary_bucket_replication" {
+  provider = aws.primary
+
+  name               = "${aws_s3_bucket.primary.id}-replication"
+  assume_role_policy = data.aws_iam_policy_document.primary_bucket_replication_role.json
+  
+  tags = var.tags
+}
+data "aws_iam_policy_document" "primary_bucket_replication" {
+  provider = aws.primary
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:GetReplicationConfiguration",
+      "s3:ListBucket",
+    ]
+
+    resources = [aws_s3_bucket.primary.arn]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObjectVersionForReplication",
+      "s3:GetObjectVersionAcl",
+      "s3:GetObjectVersionTagging",
+    ]
+
+    resources = ["${aws_s3_bucket.primary.arn}/*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:ReplicateObject",
+      "s3:ReplicateDelete",
+      "s3:ReplicateTags",
+    ]
+
+    resources = ["${aws_s3_bucket.secondary.arn}/*"]
+  }
+}
+resource "aws_iam_policy" "primary_bucket_replication" {
+  provider = aws.primary
+
+  name   = "${aws_s3_bucket.primary.id}-replication"
+  policy = data.aws_iam_policy_document.primary_bucket_replication.json
+
+  tags = var.tags
+}
+resource "aws_iam_role_policy_attachment" "primary_bucket_replication" {
+  provider = aws.primary
+
+  role       = aws_iam_role.primary_bucket_replication.name
+  policy_arn = aws_iam_policy.primary_bucket_replication.arn
+}
+
+output "primary_aws_s3_bucket_name" {
+  value = aws_s3_bucket.primary.id
+}
+
+
+resource "aws_s3_bucket" "secondary" {
+  provider = aws.secondary
+
+  bucket = var.secondary_bucket_name
+  
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = var.tags
+}
+resource "aws_s3_bucket_public_access_block" "secondary" {
+  provider = aws.secondary
+
+  bucket = aws_s3_bucket.secondary.id
+  block_public_acls = true
+  block_public_policy = true
+  restrict_public_buckets = true
+  ignore_public_acls = true
+}
+resource "aws_s3_bucket_acl" "secondary" {
+  provider = aws.secondary
+
+  bucket = aws_s3_bucket.secondary.id
+  acl    = "private"
+}
+resource "aws_s3_bucket_versioning" "secondary" {
+  provider = aws.secondary
+
+  bucket = aws_s3_bucket.secondary.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_replication_configuration" "secondary" {
+  provider = aws.secondary
+
+  depends_on = [aws_s3_bucket_versioning.secondary]
+
+  role   = aws_iam_role.secondary_bucket_replication.arn
+  bucket = aws_s3_bucket.secondary.id
+
+  rule {
+    id = "all"
+
+    delete_marker_replication {
+      status = "Enabled"
+    }
+
+    status = "Enabled"
+
+    destination {
+      bucket        = aws_s3_bucket.primary.arn
+      storage_class = "STANDARD"
+    }
+  }
+}
+data "aws_iam_policy_document" "secondary_bucket_replication_role" {
+  provider = aws.secondary
+
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["s3.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+resource "aws_iam_role" "secondary_bucket_replication" {
+  provider = aws.secondary
+
+  name               = "${aws_s3_bucket.secondary.id}-replication"
+  assume_role_policy = data.aws_iam_policy_document.secondary_bucket_replication_role.json
+
+  tags = var.tags
+}
+data "aws_iam_policy_document" "secondary_bucket_replication" {
+  provider = aws.secondary
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:GetReplicationConfiguration",
+      "s3:ListBucket",
+    ]
+
+    resources = [aws_s3_bucket.secondary.arn]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObjectVersionForReplication",
+      "s3:GetObjectVersionAcl",
+      "s3:GetObjectVersionTagging",
+    ]
+
+    resources = ["${aws_s3_bucket.secondary.arn}/*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:ReplicateObject",
+      "s3:ReplicateDelete",
+      "s3:ReplicateTags",
+    ]
+
+    resources = ["${aws_s3_bucket.primary.arn}/*"]
+  }
+}
+resource "aws_iam_policy" "secondary_bucket_replication" {
+  provider = aws.secondary
+
+  name   = "${aws_s3_bucket.secondary.id}-replication"
+  policy = data.aws_iam_policy_document.secondary_bucket_replication.json
+
+  tags = var.tags
+}
+resource "aws_iam_role_policy_attachment" "secondary_bucket_replication" {
+  provider = aws.secondary
+
+  role       = aws_iam_role.secondary_bucket_replication.name
+  policy_arn = aws_iam_policy.secondary_bucket_replication.arn
+}
+
+output "secondary_aws_s3_bucket_name" {
+  value = aws_s3_bucket.secondary.id
+}
--- variables.tf
+++ variables.tf
@@ -1,3 +1,21 @@
+variable "primary_bucket_name" {
+  description = "The name of the primary bucket. If omitted, Terraform will assign a random, unique name. Must be less than or equal to 63 characters in length."
+  type = string
+  validation {
+    condition = length(var.primary_bucket_name) >= 10 && length(var.primary_bucket_name) <= 63
+    error_message = "The name should be between 10 and 63 characters long(inclusive)."
+  }
+}
+
+variable "secondary_bucket_name" {
+  description = "The name of the secondary bucket. If omitted, Terraform will assign a random, unique name. Must be less than or equal to 63 characters in length."
+  type = string
+  validation {
+    condition = length(var.secondary_bucket_name) >= 10 && length(var.secondary_bucket_name) <= 63
+    error_message = "The name should be between 10 and 63 characters long(inclusive)."
+  }
+}
+
 variable "iam_path" {
   description = "Path name to store IAM policies and groups."
   type        = string
