7037f459c913ff9f0aa584502ca4632154716d76
gavanlamb <gavanlamb@users.noreply.github.com>
2026-05-31 23:18:30 +1000

    fix: update AWS and GitHub provider versions to latest stable releases

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.62.0"
+      version = "6.47.0"
       configuration_aliases = [
         aws.primary,
         aws.secondary
